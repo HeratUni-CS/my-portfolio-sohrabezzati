363f683834710193a3cb47c875e04d75b0c1211c
sohrabezzati <s.ezzati.30.dec.1999@gmail.com>
2023-05-05 15:26:29 +0430

    Text Added

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,26 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/1.png'),
-            )
-          ],
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/1.png'),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
